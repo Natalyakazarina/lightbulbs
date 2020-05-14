74ac51ff2a42ee2f3769cd2a2b0b8ac1f299d757
Natalyakazarina <natalya.kazarina@list.ru>
2020-05-14 14:07:11 +0300

    amedments to components

--- src/components/Light.js
+++ src/components/Light.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Light({ lightbulbs, onItemClicked }) {
   return (
-    <div className="container">
+    <div className="container-lights">
       {lightbulbs.map(({ id, flashed }) => (
         <div className="lightbulbs" key={id}>
           {flashed === true ? (
-            <img
+            <img onClick={onItemClicked}
               className="switched-off"
               src="switched_off.png"
               alt="выключенная лампочка"
             />
           ) : (
-            <img 
+            <img onClick={onItemClicked}
             className="flash" 
             src="flash.png" 
             alt="включенная лампочка" 
